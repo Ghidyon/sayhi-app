7420855d07c9028e42bd42d4f62ce71bd5ce2eae
Gideon Akunana <gideonakunana@gmail.com>
2021-01-12 12:21:44 +0100

    updated footer section with include

--- home.php
+++ home.php
@@ -48,59 +48,10 @@
     </div>
 
     <!-- Footer Section -->
+    <?php 
+        require_once(APP_ROOT . DS . 'includes' . DS . 'footer.php');
+    ?>
 
-    <footer class="page-footer darkBlue">
-        <img class="wave responsive-img" src="images/wave.svg" alt="">
-        <div class="container">
-            <div class="row">
-                <div class="col s12 stay-centered">
-                    <img class="logo" src="images/sayhi-logo.png" alt="">
-                </div>
-                <div class="col offset-s2 s3 m5 footer-margin">
-                    <ul>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Features</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Security</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Business</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Privacy</a></li>
-                    </ul>
-                </div>
-                <div class="col m4 hide-on-small-only footer-margin">
-                    <ul>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">About</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Careers</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Get in Touch</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Blog</a></li>
-                    </ul>
-                </div>
-                <div class="col offset-s2 s4 offset-m1 m2 footer-margin">
-                    <ul>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">FAQ</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Help</a></li>
-                        <li class="margin-down"><a href="#!" class="lightBlue-text uppercase">Community</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="footer-copyright darkBlue">
-                <div class="col s2">
-                    <h6 class="lightBlue-text footer-margin">© 2020 - <?= date('Y'); ?> Ghidyon</h6>
-                </div>
-                <div class="col footer-margin">
-                    <a href="#!"
-                        class="right-margin btn-large waves-light waves-effect lightBlue btn-floating footer-btn">
-                        <img class="footer-icon" src="images/facebook-f.svg" alt="">
-                    </a>
-                    <a href="#!" class="btn-large waves-light waves-effect lightBlue btn-floating footer-btn"><img
-                            class="footer-icon" src="images/twitter.svg" alt=""></a>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
-    <!-- Scripts -->
-    <script src="<?= ROOT ?>js/jquery-min.js"></script>
-    <script src="<?= ROOT ?>js/materialize.min.js"></script>
-    <script src="<?= ROOT ?>js/main.js"></script>
 </body>
 
 </html>

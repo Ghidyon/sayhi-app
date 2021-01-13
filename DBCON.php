8cdf6b9033040f820bd7f5a997b3adcca4895ab1
Gideon Akunana <gideonakunana@gmail.com>
2021-01-13 14:56:14 +0100

    added DBCON.php

---
+++ DBCON.php
@@ -0,0 +1,15 @@
+<?php
+
+// Create connection
+
+$conn = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
+
+// Check connection 
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error); // When there is an error, run this!
+}
+echo 'Connected successfully';
+var_dump($conn);
+return $conn;
+?>
--- app.php
+++ app.php
@@ -6,16 +6,17 @@
     define('DS', DIRECTORY_SEPARATOR); // for a default seperator in the user environment
     
     // Database Connection
-    define('HOST', '127.0.0.1');
-    define('DBNAME', '');
-    define('USERNAME', '');
+    define('HOST', '127.0.0.1'); // server_name
+    define('USERNAME', 'root');
     define('PASSWORD', '');
+    define('DBNAME', 'hiapp_user_data');
     // define('ROOT', '');
     
     // folder handle
     define('SITE_NAME', 'SayHi');
     define('ROOT', '/hiapp/'); // change to '/' when online
     
-    // getting scripts for actions
+    // getting scripts for 
+    require_once(APP_ROOT . DS . 'DBCON.php')
     require_once(APP_ROOT . DS . 'actions' . DS . 'functions.php');
 ?>
